312dc7980527cbab9acedb48899825b26e0af711
高雅喆 <gaoyazhe@igengmei.com>
2019-10-23 18:48:26 +0800

    update

--- syntax/recursion.py
+++ syntax/recursion.py
@@ -1,9 +1,13 @@
 # 递归(recursion)
 
+
 def list_sum(nums):
     if len(nums) == 1:
         return nums[0]
     else:
         return nums[0] + list_sum(nums[1:])
 
-print(list_sum([1,3,5,7,9]))
+
+if __name__ == "__main__":
+    print(list_sum([1, 3, 5, 7, 9]))
+
