2236dbdf562f66b37b3483a5ef5ef9583b7380af
高雅喆 <gaoyazhe@igengmei.com>
2019-10-28 21:20:15 +0800

    递归的遍历二叉树

--- syntax/recursion.py
+++ syntax/recursion.py
@@ -1,7 +1,42 @@
 # 递归(recursion)
 
 
+class BinaryTree:
+    def __init__(self, key):
+        self.val = key
+        self.left = None
+        self.right = None
+
+
+# 二叉树的递归遍历
+def preorder(root):
+    # 先序遍历
+    if root:
+        print(root.val)
+        preorder(root.left)
+        preorder(root.right)
+
+
+def inorder(root):
+    # 中序遍历
+    if root:
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
+
+
+def postorder(root):
+    # 后序遍历
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.val)
+
+# 栈遍历
+
+
 def list_sum(nums):
+    # 递归求数组的和
     if len(nums) == 1:
         return nums[0]
     else:
@@ -11,3 +46,16 @@ def list_sum(nums):
 if __name__ == "__main__":
     print(list_sum([1, 3, 5, 7, 9]))
 
+    root_test = BinaryTree(1)
+    root_test.left = BinaryTree(2)
+    root_test.right = BinaryTree(3)
+    root_test.left.left = BinaryTree(4)
+    root_test.left.right = BinaryTree(5)
+    print("Preorder traversal of binary tree is")
+    preorder(root_test)
+
+    print("Inorder traversal of binary tree is")
+    inorder(root_test)
+
+    print("Postorder traversal of binary tree is")
+    postorder(root_test)
