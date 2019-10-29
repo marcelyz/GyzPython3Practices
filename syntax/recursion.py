e2900e60a61eab831d85abdc2182960223306267
高雅喆 <gaoyazhe@igengmei.com>
2019-10-29 21:47:09 +0800

    二叉树的栈遍历

--- syntax/recursion.py
+++ syntax/recursion.py
@@ -29,7 +29,57 @@ def postorder_recursion(root):  # 后序遍历
         postorder_recursion(root.right)
         print(root.val)
 
+
 # 二叉树的栈遍历
+def preorder_stack(root):
+    node_stack = [root]
+    while len(node_stack) > 0:
+        node = node_stack.pop()
+        if node is not None:
+            print(node.val)
+        if node.right is not None:
+            node_stack.append(node.right)
+        if node.left is not None:
+            node_stack.append(node.left)
+
+
+def inorder_stack(root):
+    node_stack = []
+    node = root
+    while True:
+        if node is not None:
+            node_stack.append(node)
+            node = node.left
+        elif node_stack:
+            node = node_stack.pop()
+            print(node.val)
+            node = node.right
+        else:
+            break
+
+
+def postorder_stack(root):
+    def peek(stack):
+        if len(stack) > 0:
+            return stack[-1]
+        return None
+    node_stack = []
+    while True:
+        while root:
+            if root.right is not None:
+                node_stack.append(root.right)
+            node_stack.append(root)
+            root = root.left
+        root = node_stack.pop()
+        if root.right is not None and peek(node_stack) == root.right:
+            node_stack.pop()
+            node_stack.append(root)
+            root = root.right
+        else:
+            print(root.val)
+            root = None
+        if len(node_stack) <= 0:
+            break
 
 
 def list_sum(nums):
@@ -43,12 +93,12 @@ if __name__ == "__main__":
     # 递归求数组的和
     print(list_sum([1, 3, 5, 7, 9]))
 
+    # 二叉树的递归遍历
     root_test = BinaryTree(1)
     root_test.left = BinaryTree(2)
     root_test.right = BinaryTree(3)
     root_test.left.left = BinaryTree(4)
     root_test.left.right = BinaryTree(5)
-    # 二叉树的递归遍历
     print("preorder_recursion traversal of binary tree is")
     preorder_recursion(root_test)
     print("inorder_recursion traversal of binary tree is")
@@ -56,3 +106,10 @@ if __name__ == "__main__":
     print("postorder_recursion traversal of binary tree is")
     postorder_recursion(root_test)
     # 二叉树的栈遍历
+    print("preorder_stack traversal of binary tree is")
+    preorder_stack(root_test)
+    print("inorder_stack traversal of binary tree is")
+    inorder_stack(root_test)
+    print("postorder_stack traversal of binary tree is")
+    postorder_stack(root_test)
+
