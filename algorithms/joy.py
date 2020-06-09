c3cd2903be40e71aebc9fdb39b3a6f9a55f753ab
gaoyazhe <gaoyazhe@jd.com>
2020-06-09 16:12:44 +0800

    update

--- algorithms/joy.py
+++ algorithms/joy.py
@@ -1,6 +1,64 @@
 from typing import List
 
 
+class Solution:
+    # 大数加法(循环+进位)
+    @staticmethod
+    def big_num_add(big_num1: str, big_num2: str) -> str:
+        if len(big_num1) < len(big_num2):
+            big_num1, big_num2 = big_num2, big_num1
+        n1 = len(big_num1)
+        n2 = len(big_num2)
+        diff = n1 - n2
+        result = ""
+        carry = 0
+        # 列竖式计算过程
+        for i in range(n2-1, -1, -1):
+            n_sum = int(big_num1[i+diff]) + int(big_num2[i]) + carry
+            carry = n_sum // 10
+            result += str(n_sum % 10)
+        for i in range(n1-n2-1, -1, -1):
+            n_sum = int(big_num1[i]) + carry
+            carry = n_sum // 10
+            result += str(n_sum % 10)
+        if carry:
+            result += str(carry)
+        return result[::-1]
+
+    # 快速排序(分治+递归)
+    @staticmethod
+    def partition(nums_list: list, lo: int, hi: int) -> int:
+        threshold = lo - 1
+        pivot = nums_list[hi-1]
+        for i in range(lo, hi):
+            if nums_list[i] < pivot:
+                threshold += 1
+                nums_list[i], nums_list[threshold] = nums_list[threshold], nums_list[i]
+        nums_list[threshold+1], nums_list[hi-1] = nums_list[hi-1], nums_list[threshold+1]
+        return threshold+1
+
+    def quick_sort(self, nums_list: list, lo: int, hi: int):
+        if lo < hi:
+            pivot = self.partition(nums_list, lo, hi)
+            self.quick_sort(nums_list, lo, pivot)
+            self.quick_sort(nums_list, pivot+1, hi)
+            return
+
+    # 第k大数(类快排思想)
+    def find_k_max(self, num_list: list, lo: int, hi: int, k: int):
+        pivot = self.partition(num_list, lo, hi)
+        if pivot == k-1:
+            return num_list[pivot]
+        elif pivot < k-1:
+            return self.find_k_max(num_list, pivot, hi, k)  # 注意加return !!!
+        else:
+            return self.find_k_max(num_list, lo, pivot, k)  # 注意加return !!!
+
+    # todo: 第k大数(最小堆)
+    def find_k_max_using_min_heap(self, num_list: list, lo: int, hi: int, k: int):
+        pass
+
+
 class LinkedNode:
     def __init__(self, x: int):
         self.val = x
@@ -213,64 +271,6 @@ class BinaryTree:
         if BinaryTree.is_same_tree(t_head1, t_head2):
             return True
         return BinaryTree.is_sub_tree(t_head1, t_head2.left) or BinaryTree.is_sub_tree(t_head1, t_head2.right)
-
-
-class Solution:
-    # 大数加法(循环+进位)
-    @staticmethod
-    def big_num_add(big_num1: str, big_num2: str) -> str:
-        if len(big_num1) < len(big_num2):
-            big_num1, big_num2 = big_num2, big_num1
-        n1 = len(big_num1)
-        n2 = len(big_num2)
-        diff = n1 - n2
-        result = ""
-        carry = 0
-        # 列竖式计算过程
-        for i in range(n2-1, -1, -1):
-            n_sum = int(big_num1[i+diff]) + int(big_num2[i]) + carry
-            carry = n_sum // 10
-            result += str(n_sum % 10)
-        for i in range(n1-n2-1, -1, -1):
-            n_sum = int(big_num1[i]) + carry
-            carry = n_sum // 10
-            result += str(n_sum % 10)
-        if carry:
-            result += str(carry)
-        return result[::-1]
-
-    # 快速排序(分治+递归)
-    @staticmethod
-    def partition(nums_list: list, lo: int, hi: int) -> int:
-        threshold = lo - 1
-        pivot = nums_list[hi-1]
-        for i in range(lo, hi):
-            if nums_list[i] < pivot:
-                threshold += 1
-                nums_list[i], nums_list[threshold] = nums_list[threshold], nums_list[i]
-        nums_list[threshold+1], nums_list[hi-1] = nums_list[hi-1], nums_list[threshold+1]
-        return threshold+1
-
-    def quick_sort(self, nums_list: list, lo: int, hi: int):
-        if lo < hi:
-            pivot = self.partition(nums_list, lo, hi)
-            self.quick_sort(nums_list, lo, pivot)
-            self.quick_sort(nums_list, pivot+1, hi)
-            return
-
-    # 第k大数(类快排思想)
-    def find_k_max(self, num_list: list, lo: int, hi: int, k: int):
-        pivot = self.partition(num_list, lo, hi)
-        if pivot == k-1:
-            return num_list[pivot]
-        elif pivot < k-1:
-            return self.find_k_max(num_list, pivot, hi, k)  # 注意加return !!!
-        else:
-            return self.find_k_max(num_list, lo, pivot, k)  # 注意加return !!!
-
-    # todo: 第k大数(最小堆)
-    def find_k_max_using_min_heap(self, num_list: list, lo: int, hi: int, k: int):
-        pass
 
 
 if __name__ == "__main__":
